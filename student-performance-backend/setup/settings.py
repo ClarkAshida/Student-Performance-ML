3fd1205f7eabd37e2c04a9a9c0389d5d084000a8
ClarkAshida <flavioalexandre115@gmail.com>
2025-03-13 00:29:47 -0300

    feat: preparing settings for backend deployment

--- student-performance-backend/setup/settings.py
+++ student-performance-backend/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -132,7 +134,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Se seu frontend estiver rodando no React/Vue
+    "http://localhost:5173",  # Se seu frontend estiver rodando no React Vite
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
